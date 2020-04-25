14c63f515cb6a5509e7b064678f45600cb88a24b
陈睿 <981470928@qq.com>
2020-04-25 15:16:29 +0800

    feat：第二次提交

--- stroage/session.js
+++ stroage/session.js
@@ -1,80 +1,96 @@
 //这两个函数可以拥有每一页面标识，而且储存的数据可以是基本数据类型加数组和对象。
 //临时缓存函数
-var SessionS = (function ($) {
-    var storage = function (name) {
-        this.name = name;
-    };
-    storage.prototype.set = function (tips,item) {
-        $.setItem(this.name+':'+tips,JSON.stringify(item))
-    };
-    storage.prototype.get = function (tips) {
-        return !$.getItem(this.name+':'+tips ) ? null :JSON.parse($.getItem(this.name+':'+tips ));
-    };
-    storage.prototype.remove = function (tips) {
-        $.removeItem(this.name+':'+tips)
-    };
-    storage.prototype.getAll = function () {
-        var params = {};
-        for(var i = 0;i<$.length;i++){
-            if($.key(i).indexOf(this.name)===0){
-                var name = $.key(i).replace(this.name + ":",'');
-                var value = $.getItem($.key(i));
-                params[name] = value
-            }
-        }
-        return params
-    };
-    storage.prototype.clear = function () {
-        var arr=[];
-        var len = $.length;
-        for(var i = 0;i<len;i++){
-            if($.key(i).indexOf(this.name)===0){
-                arr.push($.key(i))
-            }
-        }
-        for(var j = 0;j<len;j++){
-            $.removeItem(arr[j])
-        }
-    };
-    return storage
-})(window.sessionStorage);
 
-//长时间缓存函数
-var LocalS = (function ($) {
-    var storage = function (name) {
-        this.name = name;
-    };
-    storage.prototype.set = function (tips,item) {
-        $.setItem(this.name+':'+tips,JSON.stringify(item))
-    };
-    storage.prototype.get = function (tips) {
-        return !$.getItem(this.name+':'+tips ) ? null :JSON.parse($.getItem(this.name+':'+tips ));
-    };
-    storage.prototype.remove = function (tips) {
-        $.removeItem(this.name+':'+tips)
-    };
-    storage.prototype.getAll = function () {
-        var params = {};
-        for(var i = 0;i<$.length;i++){
-            if($.key(i).indexOf(this.name)===0){
-                var name = $.key(i).replace(this.name + ":",'');
-                var value = $.getItem($.key(i));
-                params[name] = value
+(function (global) {
+    function reg(rege){
+        var test = {
+            a: /^\w+$/i,
+        }
+        return test[rege]
+    }
+
+    function deepGet(object, path, defaultValue) {
+        return (!Array.isArray(path) ? path.replace(/\[/g, '.').replace(/\]/g, '').split('.') : path)
+            .reduce((o, k) => (o || {})[k], object) || defaultValue;
+    }
+
+    function deepSet(object, path, value) {
+        (!Array.isArray(path) ? path.replace(/\[/g, '.').replace(/\]/g, '').split('.') : path)
+            .reduce(function (o, k, i, arr){
+                if(k === arr[arr.length-1]){
+                    o[k] = value
+                }
+                return o[k]
+
+            } , object)
+        return object
+    }
+
+    var storage = (function ($) {
+        var _ = function (name) {
+            if(name){
+                reg("a").test(name)
+                    ?
+                    this.name = name
+                    :
+                    console.error('this package name must be Obey variable naming rules')
+            }else{
+                this.name = "A"
             }
-        }
-        return params
-    };
-    storage.prototype.clear = function () {
-        var arr=[];
-        var len = $.length;
-        for(var i = 0;i<len;i++){
-            if($.key(i).indexOf(this.name)===0){
-                arr.push($.key(i))
+        };
+        _.prototype.set = function (tips,item) {
+            if(/\[|\./g.test(tips)){
+                let obj ={}
+                let trueObj = tips.split("[")[0].split('.')[0]
+                obj[trueObj] = !$.getItem(this.name+':'+trueObj ) ? null :JSON.parse($.getItem(this.name+':'+trueObj ))
+                $.setItem(this.name+':'+trueObj,JSON.stringify(
+                    deepSet(obj,tips,item)
+                ))
+            }else{
+                $.setItem(this.name+':'+tips,JSON.stringify(item))
             }
-        }
-        for(var j = 0;j<len;j++){
-            $.removeItem(arr[j])
-        }
-    };
-    return storage
-})(window.localStorage);
+        };
+        _.prototype.get = function (tips,defaultValue) {
+            let trueObj = tips.split("[")[0].split('.')[0]
+            if(tips){
+                let obj ={}
+                obj[trueObj] = !$.getItem(this.name+':'+trueObj ) ? null :JSON.parse($.getItem(this.name+':'+trueObj ))
+                defaultValue = defaultValue || null
+                return obj ? deepGet(obj,tips,defaultValue) : null
+            }
+        };
+        _.prototype.remove = function (tips,pipe) {
+            $.removeItem(this.name+':'+tips)
+        };
+        _.prototype.getAll = function () {
+            var params = {};
+            for(var i = 0;i<$.length;i++){
+                if($.key(i).indexOf(this.name)===0){
+                    var name = $.key(i).replace(this.name + ":",'');
+                    var value = $.getItem($.key(i));
+                    params[name] = JSON.parse(value)
+                }
+            }
+            return params
+        };
+        _.prototype.clear = function () {
+            var arr=[];
+            var len = $.length;
+            for(var i = 0;i<len;i++){
+                if($.key(i).indexOf(this.name)===0){
+                    arr.push($.key(i))
+                }
+            }
+            for(var j = 0;j<len;j++){
+                $.removeItem(arr[j])
+            }
+        };
+        return _
+    })
+
+    return {
+        SessionS : storage(global.sessionStorage),
+        LocalS : storage(global.localStorage)
+    }
+
+})(window)
